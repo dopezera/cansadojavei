732a3f6c08185b1dc835a55aff5db7099fd62c9b
dopezera <dopezera@gmail.com>
2021-05-01 14:54:03 -0300

    build?2

--- server.js
+++ server.js
@@ -22,9 +22,7 @@ app.use(bodyParser.json());
 
 //testando
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+
 
 app.get('/api/matches', expressAsyncHandler(async (req, res) => {
     const [results, metadata] = await sequelize.query("SELECT * FROM matches INNER JOIN pstats ON matches.id = pstats.matchId");
