383755cdb05dcf72d687f971a9cafbf96299f994
dopezera <dopezera@gmail.com>
2021-05-01 14:56:19 -0300

    build?3

--- server.js
+++ server.js
@@ -20,9 +20,6 @@ app.use(express.json()); //allow json in the body of requests (signin backend in
 app.use(express.urlencoded({ extended: true })); //with this 2 middleware all requests that contain data will translate to req.body
 app.use(bodyParser.json());
 
-//testando
-app.use(express.static(path.join(__dirname, 'build')));
-
 
 app.get('/api/matches', expressAsyncHandler(async (req, res) => {
     const [results, metadata] = await sequelize.query("SELECT * FROM matches INNER JOIN pstats ON matches.id = pstats.matchId");
